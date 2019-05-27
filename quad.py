41adac0e2b342195dc160d732ed944971c1914b0
Mattson Thieme <mattsonthieme@Mattsons-MacBook-Pro.local>
2019-05-27 14:53:37 -0700

    Use PIL to show an image with x% of pixels blanked out

--- quad.py
+++ quad.py
@@ -1,25 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from PIL import Image
 
-## Generate random distributions, zip them together
-rng = 5000
-x = [random.uniform(0,1) for i in range(rng)]
-y = [random.uniform(0,1) for i in range(rng)]
-z = list(zip(x,y))
+## Define size of the image, multiply by 255 to make the base image white
+size = 500
+data = np.ones((size,size), dtype=np.uint8)
+data *= 255
 
-## Check if each point is within the circle
-z = [i for i in z if (np.sqrt(i[0]**2 + i[1]**2)) < 1]
+## Set fraction of pixels to be blanked
+frac = 0.40
 
-## Split new zipped list back into lists we can plot
-x = [i[0] for i in z]
-y = [i[1] for i in z]
+num = int(size*size*frac)  # number of pixels to set to zero based on frac
+x = random.sample(range(size*size), num)  # non-repeating random x points
+y = random.sample(range(size*size), num)  # non-repeating random y points
+pts = list(zip(x, y))
 
-## Plotting
-limits = 1
-plt.scatter(x,y)
-plt.axis('equal')  # Keeps the plot a square - kindof works
-plt.xlim(0,limits)
-plt.ylim(0,limits)
+## Blank pixels defined in pts
+## take i[0] modulo size as a single row/column could have multiple blanked pixels
+for i in pts:
+	data[i[0]%size][i[1]%size] = 0
 
-plt.show()
+## Set all pixels outside the circle to zero
+for row in range(size):
+	for col in range(size):
+		if (np.sqrt(row**2 + col**2)) >= size:
+			data[row][col] = 0
+
+img = Image.fromarray(data, 'L')
+img.show()
