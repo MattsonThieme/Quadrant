ecb01eae2ecc7b2ab86c765f1b3ca1620d74c9d0
Mattson Thieme <mattsonthieme@Mattsons-MacBook-Pro.local>
2019-05-27 15:40:06 -0700

    Fix degenerate pixels bug

--- quad.py
+++ quad.py
@@ -9,17 +9,17 @@ data = np.ones((size,size), dtype=np.uint8)
 data *= 255
 
 ## Set fraction of pixels to be blanked
-frac = 0.40
-
+frac = 0.65
 num = int(size*size*frac)  # number of pixels to set to zero based on frac
-x = random.sample(range(size*size), num)  # non-repeating random x points
-y = random.sample(range(size*size), num)  # non-repeating random y points
-pts = list(zip(x, y))
+pts = random.sample(range(size*size), num)
 
 ## Blank pixels defined in pts
-## take i[0] modulo size as a single row/column could have multiple blanked pixels
+## Basically, pts is a 1D array of values between 0 and the total number of pixels.
+## To blank the pixels, I'm wrapping those values around. FYI: the int function runds down.
+## For example: if size=50 and some value in pts is 104, we'll blank out the pixel in
+## the third row [int(i/size)] and fourth column [i-size*int(i/size)]
 for i in pts:
-	data[i[0]%size][i[1]%size] = 0
+	data[int(i/size)][i-size*int(i/size)] = 0
 
 ## Set all pixels outside the circle to zero
 for row in range(size):
